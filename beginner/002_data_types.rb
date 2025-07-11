5b0cdbc989f246faf9ebd2b12159c38712c1da91
Kiran Kumar V <imkirankumar23@gmail.com>
2025-07-11 20:26:41 +0530

    Create 002_data_types.rb

---
+++ beginner/002_data_types.rb
@@ -0,0 +1,35 @@
+# Basic Data Types in Ruby
+
+# 1. Numbers
+integer = 42
+float = 3.14
+
+# 2. Strings
+string = "Hello, Ruby!"
+single_quoted = 'Single quotes'
+
+# 3. Booleans
+true_value = true
+false_value = false
+
+# 4. Nil
+nothing = nil
+
+# 5. Symbols
+symbol = :ruby_symbol
+
+# 6. Arrays
+array = [1, "two", 3.0, :four]
+
+# 7. Hashes
+hash = { key1: "value1", key2: "value2" }
+
+puts "Integer: #{integer}"
+puts "Float: #{float}"
+puts "String: #{string}"
+puts "Boolean true: #{true_value}"
+puts "Boolean false: #{false_value}"
+puts "Nil: #{nothing.inspect}"
+puts "Symbol: #{symbol}"
+puts "Array: #{array}"
+puts "Hash: #{hash}"
